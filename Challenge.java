2f64f6faa8fde29175a4597ce8a5d621382389bd
EduFranco <72152673+du-franco@users.noreply.github.com>
2022-03-30 16:06:29 -0300

    Add files via upload

---
+++ Challenge.java
@@ -0,0 +1,12 @@
+public class Challenge {
+	public static String rps(String s1, String s2) {
+		if (s1 == s2) {
+			return "TIE";
+		} else if ((s1 == "rock" && s2 == "scissors") || (s1 == "scissors" && s2 == "paper")
+				|| (s1 == "paper" && s2 == "rock")) {
+			return "Player 1 wins";
+		} else {
+			return "Player 2 wins";
+		}
+	}
+}
